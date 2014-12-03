7267988075a83f5b6d1fd0cf93b227774443fb24
HAN <penguns@naver.com>
2014-12-03 09:13:42 +0900

    Release 0.1.0

--- dist/angular-bespoke.js
+++ dist/angular-bespoke.js
@@ -375,42 +375,23 @@ angular.module('artishan.bespoke', []).directive('bespoke', [ function () {
 	return {
 		restrict: 'AE',
 		scope: {
+			bespokeSlides: '=slides',
 		},
-
-		// replace: true,
-		template: function(element, attrs) {
-			var defaultsAttrs ={
-			};
-			for(var xx in defaultsAttrs) {
-				if(attrs[xx] ===undefined) {
-					attrs[xx] =defaultsAttrs[xx];
-				}
-			}
-			console.log(element);
-			var html = "<section>Slide 1</section>";
-					html += "<section>Slide 2</section>";
-					html += "<section>Slide 3</section>";
-			// var html = "<article id='presentation'>";
-			// 		html += "<section>Slide 1</section>";
-			// 		html += "<section>Slide 2</section>";
-			// 		html += "<section>Slide 3</section>";
-			// 		html += "</article>";
-			console.log(html);
-			return html;
-		},
-
-		link: function(scope, element, attrs) {
-
-		},
-
-		controller: function($scope, $element, $attrs) {
-			var deck = bespoke.from($element[0], [
-				bespoke.themes.cube(),
-				bespoke.plugins.classes(),
-  			bespoke.plugins.keys(),
-				bespoke.plugins.scale(),
-				bespoke.plugins.progress()
-			]);
-		}
+    link: function(scope, elem, attrs) {
+			var slides = scope.bespokeSlides;
+      if(slides !== undefined){
+        angular.forEach(slides, function(slide){
+          var section = angular.element("<section>" + slide + "<section>");
+          elem.append(section);
+        });
+				bespoke.from(elem[0], [
+					bespoke.themes.cube(),
+					bespoke.plugins.classes(),
+					bespoke.plugins.keys(),
+					bespoke.plugins.scale(),
+					bespoke.plugins.progress()
+				]);
+      }
+    }
 	};
 }]);
